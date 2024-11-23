d0b18569fe49d1e85627d48024b9817b516ac16a
vladriabyi <whiteboys100@gmail.com>
2024-11-23 13:00:39 +0200

    created the BuildingBlock class with attributes

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    
+    public static class BuildingBlock {
+        private String name;
+        private String material;
+        private int hardness;
+        private boolean isTransparent;
+        private int stackLimit;
+
+        public BuildingBlock(String name, String material, int hardness, boolean isTransparent, int stackLimit) {
+            this.name = name;
+            this.material = material;
+            this.hardness = hardness;
+            this.isTransparent = isTransparent;
+            this.stackLimit = stackLimit;
+        }
+
+        // Getters and setters (to be added in the next commit)
+    }
+
+    public static void main(String[] args) {
+        // Main logic will be added in future commits
+    }
+}
